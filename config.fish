a8e43beefc10efeac92441515ce9522548b7f8b3
Zaeem <raozaeem2018@gmail.com>
2025-07-17 19:06:38 +0500

    Bug fixed: Incorrect PNPM_HOME path

--- config.fish
+++ config.fish
@@ -184,7 +184,7 @@ set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
 # pnpm
-set -gx PNPM_HOME "/home/zaeem/.local/share/pnpm"
+set -gx PNPM_HOME "~/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
